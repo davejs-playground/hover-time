2c67ed8990b35fdcfe4fe9c1d6823a2ecb10f3d7
David Leger <davidleger95@me.com>
2020-05-22 20:44:53 -0300

    add super awesome styles

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -4,16 +4,41 @@ import { useTimer } from './useTimer';
 import Button from '../Button';
 
 const StyledBox = styled.div`
+  --size: 200px;
+  --color: linear-gradient(45deg, var(--purple), var(--gray));
+
   display: grid;
   align-content: center;
   justify-content: center;
 
-  --size: 200px;
   width: var(--size);
   height: var(--size);
   font-size: 2.5rem;
   font-family: monospace;
-  background-color: pink;
+  background: var(--color);
+  border-radius: 2rem;
+  animation-name: pulse;
+  animation-duration: 2s;
+  animation-play-state: paused;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+
+  &:hover {
+    animation-play-state: running;
+  }
+
+  @keyframes pulse {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.05);
+    }
+    100% {
+      transform: scale(0.95);
+    }
+  }
 `;
 
 const Box = () => {
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,8 +2,17 @@ import React, { FC, ButtonHTMLAttributes } from 'react';
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
-  padding: 1em 2em;
+  padding: 1.5em 2.5em;
+  color: var(--purple);
+  font-weight: 600;
+  text-transform: uppercase;
+  border: 2px solid var(--purple);
+  border-radius: 2rem;
   appearance: none;
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 const Button: FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) => (
