2cb42639d84b8743b468284a731c4d3c2f137edb
David Leger <davidleger95@me.com>
2020-05-22 19:30:49 -0300

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: grid;
+  align-content: center;
+  justify-content: center;
+  height: 100vh;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Container>Hello</Container>;
 }
 
 export default App;
